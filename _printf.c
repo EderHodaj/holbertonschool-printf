bac17ccde03c4974c97a5043ea0d1e91abef3e97
EderHodaj <eder0302@gmail.com>
2023-11-20 02:29:09 -0800

    0

--- _printf.c
+++ _printf.c
@@ -3,5 +3,19 @@
 
 int _printf(const char *format, ...)
 {
-	printf("%s", format);
+	va_list args;
+	int len_f = 0;
+	int i;
+
+	va_start(args, format);
+
+	while (format[len_f] != '\0')
+		len_f++;
+	for (i = 0; i < len_f; i++)
+	{
+		va_arg(args, char*);
+		printf("%s", va_arg(args, char*));
+	}
+	va_end(args);
+	return(len_f);
 }
