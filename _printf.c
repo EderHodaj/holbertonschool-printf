916e96f94366c96e2677e45e8f8016dff64df6e0
eneamilo514 <eneamilo514@gmail.com>
2023-11-21 03:27:55 -0800

    anotherfile

--- _printf.c
+++ _printf.c
@@ -1,21 +1,42 @@
 #include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+
 
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int len_f = 0;
-	int i;
+	int i = 0;
+	int (*func)(va_list);
+	int count = 0;
+	int length;
 
-	va_start(args, format);
-
-	while (format[len_f] != '\0')
-		len_f++;
-	for (i = 0; i < len_f; i++)
+	if (format != NULL)
 	{
-		va_arg(args, char*);
-		printf("%s", va_arg(args, char*));
+		length = strlen(format);
+		if ((length != 1) || (format[0] != '%'))
+		{
+			va_start(args, format);
+			while (format[i] != '\0')
+			{
+				if (format[i] == '%')
+				{
+					func = get_function(format[i + 1]);
+					if (func)
+					{
+						count = count + func(args);
+						i = i + 2;
+						continue;
+					}
+					else if (format[i + 1] == '%')
+						i++;
+				}
+				_putchar(format[i]);
+				i++;
+				count++;
+			}
+		}
 	}
-	va_end(args);
-	return(len_f);
+		return (count);
 }
